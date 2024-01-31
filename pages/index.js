683f8ce3773b3f051705813257a98bc99939f23e
tom-gospooky <74213799+tom-gospooky@users.noreply.github.com>
2024-01-31 12:08:26 +0100

    content upgrade

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,28 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.container} style={{ backgroundImage: `url('/images/background.jpg')` }}>
+      <header className={styles.header}>
+        <img src="/images/header.png" alt="Header" />
+      </header>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          <a href="/usdz/usdz_1.usdz" rel="ar" className={styles.card}>
+            <img src="/images/preview_1.png" alt="USDZ 1" />
+          </a>
+          <a href="/usdz/usdz_2.usdz" rel="ar" className={styles.card}>
+            <img src="/images/preview_2.png" alt="USDZ 1" />
+          </a>
+          <a href="/usdz/usdz_3.usdz" rel="ar" className={styles.card}>
+            <img src="/images/preview_3.png" alt="USDZ 1" />
+          </a>
+          <a href="/usdz/usdz_4.usdz" rel="ar" className={styles.card}>
+            <img src="/images/preview_4.png" alt="USDZ 1" />
+          </a>
+        </div>
       </main>
-
-      <Footer />
     </div>
-  )
+  );
 }
